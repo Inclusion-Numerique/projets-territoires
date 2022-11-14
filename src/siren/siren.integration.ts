25eb576d7f78ac7aa7aa319b918e1fa63f4085c9
Hugues Maignol <hugues@kime.tech>
2022-11-14 10:22:31 +0100

    test: siren test stub

--- src/siren/siren.integration.ts
+++ src/siren/siren.integration.ts
@@ -1,7 +1,11 @@
 import { searchCommunity } from '@pt/siren/siren'
 
 describe('siren', () => {
-  it('Searches SIREN', async () => {
+  it('Searches with multiple words', async () => {
     const result = await searchCommunity('Lyon 1er')
+  })
+
+  it('Searches without duplicates', async () => {
+    const result = await searchCommunity('Grenoble')
   })
 })
