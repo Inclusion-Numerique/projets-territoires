fcc45abf844d8ecc95e868a34b4ec1c5f25b2623
Hugues Maignol <hugues@kime.tech>
2022-11-23 11:26:34 +0100

    feat: remove opening time feature

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,19 +1,8 @@
 import { PropsWithChildren } from 'react'
 import PublicHeader from '@pt/app/(public)/PublicHeader'
 import PublicFooter from '@pt/app/(public)/PublicFooter'
-import { PrivateConfig } from '@pt/config'
-import { redirect } from 'next/navigation'
-import { headers } from 'next/headers'
 
 const PublicLayout = ({ children }: PropsWithChildren) => {
-  // Disable static rendering
-  headers()
-
-  const openingTime = PrivateConfig.openingTime
-  if (openingTime && new Date() < new Date(openingTime)) {
-    return redirect('/salon-des-maires')
-  }
-
   return (
     <div
       style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}
